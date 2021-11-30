c7819d94a8fc4ca9a68fbf6e57eac24a6bbb6596
riccardocabona <91782134+riccardocabona@users.noreply.github.com>
2021-11-30 14:47:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include"lib.h"
+bool lettera(char &a){
+    bool flag=true;
+    if((((a>=65)&&(a<=90))||((a>=97)&&(a<=122)))){
+        bool flag=true;
+        if((a>=65)&&(a<=90)){
+            a=a+32;
+            return a;
 
+        }else{
+            a=a-32;
+            return a;
+        }
+    }else{
+        flag=false;
+    }
+    return flag;
+}
